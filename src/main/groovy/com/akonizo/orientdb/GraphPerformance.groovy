888b0438c00426cc76152faa6ffb4055c0b2098f
W. Craig Trader <craig@trader.name>
2015-04-18 09:17:34 -0400

    Initial Gradle project

---
+++ src/main/groovy/com/akonizo/orientdb/GraphPerformance.groovy
@@ -0,0 +1,12 @@
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class GraphPerformance {
+
+    final static Logger LOGGER = LoggerFactory.getLogger( GraphPerformance.class )
+
+    static void main( String[] args ) {
+        LOGGER.info( "Start graph performance test" )
+        LOGGER.info( "Test complete" )
+    }
+}
