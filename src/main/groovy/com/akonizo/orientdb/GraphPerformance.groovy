1755d1b7221acfcfb43dbaae295f4778638a1c5d
W. Craig Trader <ctrader@verisign.com>
2015-04-18 13:38:56 -0400

    Tweak profiling

--- src/main/groovy/com/akonizo/orientdb/GraphPerformance.groovy
+++ src/main/groovy/com/akonizo/orientdb/GraphPerformance.groovy
@@ -2,6 +2,7 @@ package com.akonizo.orientdb
 
 import com.tinkerpop.blueprints.impls.orient.OrientBaseGraph
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
+
 import org.hyperic.sigar.Sigar
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -10,7 +11,7 @@ import org.slf4j.profiler.Profiler
 class GraphPerformance {
 
     final static Logger LOGGER = LoggerFactory.getLogger( GraphPerformance.class )
-    final static Profiler PROFILER = new Profiler( "GraphPerformance" )
+    static Profiler PROFILER
 
     Random random
     String dbpath
@@ -24,19 +25,20 @@ class GraphPerformance {
 
     def createDatabase( String path ) {
         dbpath = path
-
     }
 
     def cleanup() {
-
     }
 
     def logSystemInformation() {
+        def vendor = System.properties.get('java.vendor')
+        def version = System.properties.get( 'java.version')
+        LOGGER.info( 'Java: {} {}', vendor, version )
+
         try {
             def sigar = new Sigar()
             LOGGER.info( 'System:  {}', sigar.getCpuInfoList() )
             LOGGER.info( 'Memory:  {}', sigar.getMem() )
-
         } catch ( Exception e ) {
             LOGGER.error( "Sigar failed", e )
         }
@@ -46,15 +48,15 @@ class GraphPerformance {
         GraphPerformance gp = new GraphPerformance()
         try {
             gp.logSystemInformation()
+            PROFILER = new Profiler( "GraphPerformance" )
             gp.initialize( 123465798L )
-            gp.createDatabase("memory:test")
             PROFILER.start("Create database")
-
+            gp.createDatabase("memory:test")
         } finally {
             PROFILER.start( "Database cleanup" )
             gp.cleanup()
             PROFILER.stop()
-            LOGGER.info( "Profiler: {}", PROFILER )
+            LOGGER.info( "{}", PROFILER )
         }
     }
 }
