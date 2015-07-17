d87f3409bba036f798eb164c6065b598c6ea555a
W. Craig Trader <ctrader@verisign.com>
2015-07-17 17:04:25 -0400

    Validate advertised Java environments

--- test-environment.sh
+++ test-environment.sh
@@ -2,7 +2,7 @@
 
 validate_java() {
 	varname=$1
-	expanded_name=${!varname}
+	expanded_name=$2
 	java_pathname=${expanded_name}/bin/java
 	echo "Testing ${varname} => ${expanded_name} ... "
 	if [ -d ${expanded_name} ] ; then
@@ -31,8 +31,8 @@ java -version
 
 echo ============================================================
 echo Advertised versions of Java:
-env | egrep -i "^jdk|^java" | cut -d'=' -f1 | sort | while read var ; do
-	validate_java $var
+env | egrep -i "^jdk|^java" | sort | tr '=' ' ' | while read var pathname ; do
+	validate_java $var $pathname
 done
 
 echo ============================================================
