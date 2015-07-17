8098d191cfb3fe96231ea28dbab0e41d47bc6609
W. Craig Trader <ctrader@verisign.com>
2015-07-17 16:20:15 -0400

    Add script to test environment

--- test-environment.sh
+++ test-environment.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 echo Interesting environment variables:
-env | egrep -i 'jdk|java|groovy'
+env | egrep -i 'jdk|java|groovy' | sort
 
 echo Default Java version:
 whereis java
 java -version
 
 echo "Java Home (${JAVA_HOME})"
-if [ -x ${JAVA_HOME)/bin/java ] ; then
+if [ -x ${JAVA_HOME}/bin/java ] ; then
 	${JAVA_HOME}/bin/java -version
 fi
