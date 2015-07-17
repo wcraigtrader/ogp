5561cd9bf4a193a731e01b62d701b5a17f7d69e6
W. Craig Trader <ctrader@verisign.com>
2015-07-17 16:43:57 -0400

    Validate advertised Java environments

--- test-environment.sh
+++ test-environment.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
+validate_java() {
+	varname=$1
+	echo -n "Testing ${varname} => ${!varname} ... "
+	if [ -d ${!varname} ] ; then
+		if [ -x ${!varname}/bin/java ] ; then
+			${!varname}/bin/java -version
+		else 
+			echo "No java executable found"
+		fi
+	else 
+		echo "Not actually installed."
+	fi
+}
+
 echo ============================================================
 echo Interesting environment variables:
 env | egrep -i 'jdk|java|groovy' | sort
-
-echo ============================================================
-echo All Installed Java Versions:
-ls -l /usr/lib/jvm 
 
 echo ============================================================
 echo All Build Tools:
@@ -18,9 +28,9 @@ whereis java
 java -version
 
 echo ============================================================
-echo "Java Home (${JAVA_HOME})"
-if [ -x ${JAVA_HOME}/bin/java ] ; then
-	${JAVA_HOME}/bin/java -version
-fi
+echo Advertised versions of Java:
+env | egrep -i "^jdk|^java" | cut -d'=' -f1 | sort | while read var ; do
+	validate_java $var
+done
 
 echo ============================================================
